4bb86cfbf92245f8149c73964cf68b57e3786b70
GabeMed <gabrymedeiros7@gmail.com>
2025-04-01 00:06:40 -0300

    Adding a border to anime card component

--- Frontend/src/components/AnimeCardContainer.tsx
+++ Frontend/src/components/AnimeCardContainer.tsx
@@ -7,7 +7,14 @@ interface Props {
 
 const AnimeCardContainer = ({ children }: Props) => {
   return (
-    <Box borderRadius={10} overflow={"hidden"} height="420px" width="525px">
+    <Box
+      borderRadius={10}
+      overflow={"hidden"}
+      height="420px"
+      width="525px"
+      border="1px solid"
+      borderColor="gray.800"
+    >
       {children}
     </Box>
   );
