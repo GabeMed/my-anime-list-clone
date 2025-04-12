669d5f61269ecc906c9cb4ffe186259b3ef2f812
GabeMed <gabrymedeiros7@gmail.com>
2025-04-12 19:35:42 -0300

    Adding more anime types

--- Frontend/src/utils/animeType.tsx
+++ Frontend/src/utils/animeType.tsx
@@ -1,15 +1,37 @@
 import { IconType } from "react-icons";
-import { FaTv, FaFilm, FaCompactDisc, FaGlobe, FaStar } from "react-icons/fa";
+import {
+  FaTv,
+  FaFilm,
+  FaCompactDisc,
+  FaGlobe,
+  FaStar,
+  FaBullhorn,
+  FaMusic,
+  FaPlay,
+} from "react-icons/fa";
 
-export type AnimeType = "TV" | "Movie" | "OVA" | "ONA" | "Special";
+export type AnimeType =
+  | "TV"
+  | "OVA"
+  | "Movie"
+  | "Special"
+  | "ONA"
+  | "Music"
+  | "CM"
+  | "PV"
+  | "TV Special";
 
 export const animeTypeData: Record<
   AnimeType,
-  { color: string; icon: IconType }
+  { icon: IconType; color: string }
 > = {
-  TV: { color: "blue", icon: FaTv },
-  Movie: { color: "red", icon: FaFilm },
-  OVA: { color: "purple", icon: FaCompactDisc },
-  ONA: { color: "teal", icon: FaGlobe },
-  Special: { color: "orange", icon: FaStar },
+  TV: { icon: FaTv, color: "blue" },
+  OVA: { icon: FaCompactDisc, color: "purple" },
+  Movie: { icon: FaFilm, color: "red" },
+  Special: { icon: FaStar, color: "yellow" },
+  ONA: { icon: FaGlobe, color: "green" },
+  Music: { icon: FaMusic, color: "pink" },
+  CM: { icon: FaBullhorn, color: "orange" },
+  PV: { icon: FaPlay, color: "teal" },
+  "TV Special": { icon: FaTv, color: "cyan" },
 };
