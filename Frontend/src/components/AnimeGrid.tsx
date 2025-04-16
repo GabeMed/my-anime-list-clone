3dd786a63ec07b26ac8824afbe578cc283b6f5cf
GabeMed <gabrymedeiros7@gmail.com>
2025-04-16 16:17:53 -0300

    Refactor anime grid

--- Frontend/src/components/AnimeGrid.tsx
+++ Frontend/src/components/AnimeGrid.tsx
@@ -13,30 +13,29 @@ const AnimeGrid = ({ animeQuery }: Props) => {
   const { data, error, isLoading } = useAnimes(animeQuery);
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+  if (error) return <Text>{error}</Text>;
+
   return (
-    <>
-      {error && <Text>{error}</Text>}
-      <SimpleGrid
-        columns={{ base: 1, lg: 2, xl: 3 }} //todo adjust responsive logic
-        padding="10px"
-        columnGap={4}
-        rowGap={10}
-      >
-        {skeletons.map(
-          (skeleton) =>
-            isLoading && (
-              <AnimeCardContainer key={skeleton}>
-                <AnimeCardSkeleton />
-              </AnimeCardContainer>
-            )
-        )}
-        {data.map((anime) => (
-          <AnimeCardContainer key={anime.mal_id}>
-            <AnimeCard anime={anime} />
-          </AnimeCardContainer>
-        ))}
-      </SimpleGrid>
-    </>
+    <SimpleGrid
+      columns={{ base: 1, lg: 2, xl: 3 }} //todo adjust responsive logic
+      padding="10px"
+      columnGap={4}
+      rowGap={10}
+    >
+      {skeletons.map(
+        (skeleton) =>
+          isLoading && (
+            <AnimeCardContainer key={skeleton}>
+              <AnimeCardSkeleton />
+            </AnimeCardContainer>
+          )
+      )}
+      {data.map((anime) => (
+        <AnimeCardContainer key={anime.mal_id}>
+          <AnimeCard anime={anime} />
+        </AnimeCardContainer>
+      ))}
+    </SimpleGrid>
   );
 };
 
